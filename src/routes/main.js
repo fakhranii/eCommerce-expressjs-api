78ff3d7931670f09fd963ed3da148264ddbfbe24
fakhranyy <ziad.fakhranyy@gmail.com.com>
2024-08-07 00:57:32 +0300

    prepare to production

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,6 +1,6 @@
 import categoryRoute from "./categoryRoute.js";
 import reviewRoute from "./reviewRoute.js";
-// import subCategoryRoute from "./subCategoryRoute.js";
+import subCategoryRoute from "./subCategoryRoute.js";
 import brandRoute from "./brandRoute.js";
 import productRoute from "./productRoute.js";
 import userRoute from "./userRoute.js";
@@ -16,7 +16,7 @@ const mountRoutes = (app) => {
   app.use("/api/v1/users", userRoute);
   app.use("/api/v1/reviews", reviewRoute);
   app.use("/api/v1/categories", categoryRoute);
-  // app.use("/api/v1/subcategories", subCategoryRoute);
+  app.use("/api/v1/subcategories", subCategoryRoute);
   app.use("/api/v1/brands", brandRoute);
   app.use("/api/v1/products", productRoute);
   app.use("/api/v1/auth", authRoute);
--- src/routes/subCategoryRoute.js
+++ src/routes/subCategoryRoute.js
@@ -1,50 +1,50 @@
-// import { Router } from "express";
+import { Router } from "express";
 
-// import {
-//   createFilterObj,
-//   createSubCategory,
-//   deleteSubcategory,
-//   getSubCategories,
-//   getSubCategory,
-//   setCategoryIdToBody,
-//   updateSubcategory,
-// } from "../services/subCategoryService.js";
-// import {
-//   createSubCategoryValidator,
-//   getSubCategoryValidator,
-//   updateSubCategoryValidator,
-//   deleteSubCategoryValidator,
-// } from "../utils/validators/subCategoryValidator.js";
-// import { allowedTo, verifyToken } from "../services/authService.js";
+import {
+  createFilterObj,
+  createSubCategory,
+  deleteSubcategory,
+  getSubCategories,
+  getSubCategory,
+  setCategoryIdToBody,
+  updateSubcategory,
+} from "../services/subCategoryService.js";
+import {
+  createSubCategoryValidator,
+  getSubCategoryValidator,
+  updateSubCategoryValidator,
+  deleteSubCategoryValidator,
+} from "../utils/validators/subCategoryValidator.js";
+import { allowedTo, verifyToken } from "../services/authService.js";
 
-// //? mergeParmas -> Allow us to access params on another routers
-// const router = Router({ mergeParams: true });
+//? mergeParmas -> Allow us to access params on another routers
+const router = Router({ mergeParams: true });
 
-// router
-//   .route("/")
-//   .post(
-//     verifyToken,
-//     allowedTo("admin", "manager"),
-//     setCategoryIdToBody,
-//     createSubCategoryValidator,
-//     createSubCategory
-//   )
-//   .get(createFilterObj, getSubCategories);
-// router
-//   .route("/:id")
-//   .get(getSubCategoryValidator, getSubCategory)
-//   .patch(
-//     verifyToken,
-//     allowedTo("admin", "manager"),
-//     updateSubCategoryValidator,
-//     updateSubcategory
-//   )
-//   .delete(
-//     verifyToken,
-//     allowedTo("admin"),
-//     deleteSubCategoryValidator,
-//     deleteSubcategory
-//   );
+router
+  .route("/")
+  .post(
+    verifyToken,
+    allowedTo("admin", "manager"),
+    setCategoryIdToBody,
+    createSubCategoryValidator,
+    createSubCategory
+  )
+  .get(createFilterObj, getSubCategories);
+router
+  .route("/:id")
+  .get(getSubCategoryValidator, getSubCategory)
+  .patch(
+    verifyToken,
+    allowedTo("admin", "manager"),
+    updateSubCategoryValidator,
+    updateSubcategory
+  )
+  .delete(
+    verifyToken,
+    allowedTo("admin"),
+    deleteSubCategoryValidator,
+    deleteSubcategory
+  );
 
-// // remember to mount this routes in main file (server.js)
-// export default router;
+// remember to mount this routes in main file (server.js)
+export default router;
